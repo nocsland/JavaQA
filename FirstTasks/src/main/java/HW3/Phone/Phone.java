ee56064a00f7bba06cdc3aa723c48b0c69514ca0
nocsland <nocsland@gmail.com>
2022-02-02 08:00:46 +0400

    added comment phone class

--- FirstTasks/src/main/java/HW3/Phone/Phone.java
+++ FirstTasks/src/main/java/HW3/Phone/Phone.java
@@ -5,6 +5,7 @@ public class Phone {
     private String model;
     private Double weight;
 
+    //Геттеры и сеттеры
     public int getNumber() {
         return number;
     }
@@ -29,15 +30,17 @@ public class Phone {
         this.weight = weight;
     }
 
-
+    // Выводит имя звонящего
     public void receiveCall(String name) {
         System.out.printf("Звонит %s\n", name);
     }
 
+    // Выводит имя звонящего и номер
     public void receiveCall(String name,int incomingNumber) {
         System.out.printf("Звонит %s %d\n",name, incomingNumber);
     }
 
+    //Выводит список номеров на которые отправлено сообщение
     public void sendMessage(int[] numbers){
         for (int i:numbers){
             System.out.print(i + " ");
@@ -45,17 +48,20 @@ public class Phone {
         System.out.println("На эти номера отправлено сообщение");
     }
 
+    //Конструктор с номером, моделью и весом
     Phone(int number, String model, Double weight) {
         this.number = number;
         this.model = model;
         this.weight = weight;
     }
 
+    //Конструктор с номером и моделью
     Phone(int number, String model) {
         this.number = number;
         this.model = model;
     }
 
+    //Конструктор по умолчанию
     Phone() {
     }
 
